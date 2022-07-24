6c38b50612d6512e0873c3028e3415ece81c2506
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2022-07-24 16:20:17 +0900

    [update] #1 教科ページ仮作成

--- lib/src/screens/list.dart
+++ lib/src/screens/list.dart
@@ -6,10 +6,66 @@ class ListScreeen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("科目")),
-      body: const Center(
-        child: Text("科目画面", style: TextStyle(fontSize: 32.0)),
-      ),
-    );
+        appBar: AppBar(title: const Text("教科")),
+        body: Column(
+          children: <Widget>[
+            const Text('勉強したい教科を選択してください。'),
+            Container(
+              padding: const EdgeInsets.only(top: 32),
+            ),
+            SizedBox(
+              width: double.infinity, //横幅
+              height: 50, //高さ
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('国語'),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 32),
+            ),
+            SizedBox(
+              width: double.infinity, //横幅
+              height: 50, //高さ
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('数学'),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 32),
+            ),
+            SizedBox(
+              width: double.infinity, //横幅
+              height: 50, //高さ
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('英語'),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 32),
+            ),
+            SizedBox(
+              width: double.infinity, //横幅
+              height: 50, //高さ
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('理科'),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 32),
+            ),
+            SizedBox(
+              width: double.infinity, //横幅
+              height: 50, //高さ
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('社会'),
+              ),
+            ),
+          ],
+        ));
   }
 }

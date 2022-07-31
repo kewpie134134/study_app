71fbde4fb3b5c220ecd0ff834871778e38996265
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2022-07-31 11:24:34 +0900

    [update] #1 リストを動的に作成するよう修正

--- lib/src/screens/subject.dart
+++ lib/src/screens/subject.dart
@@ -1,77 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:study_app/src/screens/unit.dart';
 
+List<String> subjects = [
+  "国語",
+  "数学",
+  "英語",
+  "理科",
+  "社会",
+];
+
 class SubjectScreeen extends StatelessWidget {
   const SubjectScreeen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var contentWidgets = _makeWidgets(context);
+
     return Scaffold(
         appBar: AppBar(title: const Text("教科選択")),
-        body: Column(
-          children: <Widget>[
-            const Text('勉強したい教科を選んでね。'),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-            ),
-            SizedBox(
-              width: double.infinity, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) {
-                    return const UnitScreen();
-                  }));
-                },
-                child: const Text('国語'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-            ),
-            SizedBox(
-              width: double.infinity, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('数学'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-            ),
-            SizedBox(
-              width: double.infinity, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('英語'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-            ),
-            SizedBox(
-              width: double.infinity, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('理科'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-            ),
-            SizedBox(
-              width: double.infinity, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('社会'),
-              ),
-            ),
-          ],
-        ));
+        body: Column(children: contentWidgets));
   }
+}
+
+List<Widget> _makeWidgets(BuildContext context) {
+  var contentWidgets = <Widget>[];
+
+  contentWidgets.add(const Text('勉強したい教科を選んでね。'));
+  for (final subject in subjects) {
+    contentWidgets.add(Container(
+      padding: const EdgeInsets.only(top: 32),
+    ));
+    contentWidgets.add(
+      SizedBox(
+        width: double.infinity, //横幅
+        height: 50, //高さ
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return UnitScreen(subject: subject);
+            }));
+          },
+          child: Text(subject.toString()),
+        ),
+      ),
+    );
+  }
+  return contentWidgets;
 }
--- lib/src/screens/unit.dart
+++ lib/src/screens/unit.dart
@@ -1,71 +1,51 @@
 import 'package:flutter/material.dart';
 
+List<String> units = [
+  "AAAAA",
+  "BBBBB",
+  "CCCCC",
+  "DDDDD",
+  "EEEEE",
+  "FFFFF",
+  "GGGGG",
+];
+
 class UnitScreen extends StatelessWidget {
-  const UnitScreen({Key? key}) : super(key: key);
+  const UnitScreen({Key? key, required this.subject}) : super(key: key);
+  final String subject;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("単元選択")),
+        appBar: AppBar(title: Text("$subject: 単元選択")),
         body: Column(
-          children: <Widget>[
-            const Text('勉強したい単元を選んでね。'),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-            ),
-            SizedBox(
-              width: double.infinity, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('XXXXXX'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-            ),
-            SizedBox(
-              width: double.infinity, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('XXXXXX'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-            ),
-            SizedBox(
-              width: double.infinity, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('XXXXXX'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-            ),
-            SizedBox(
-              width: double.infinity, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('XXXXXX'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 32),
-            ),
-            SizedBox(
-              width: double.infinity, //横幅
-              height: 50, //高さ
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('XXXXXX'),
-              ),
-            ),
-          ],
+          children: _makeWidgets(context),
         ));
   }
+}
+
+List<Widget> _makeWidgets(BuildContext context) {
+  var contentWidgets = <Widget>[];
+
+  contentWidgets.add(const Text('勉強したい単元を選んでね。'));
+  for (final unit in units) {
+    contentWidgets.add(Container(
+      padding: const EdgeInsets.only(top: 32),
+    ));
+    contentWidgets.add(
+      SizedBox(
+        width: double.infinity, //横幅
+        height: 50, //高さ
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            //   return UnitScreen(subject: unit);
+            // }));
+          },
+          child: Text(unit.toString()),
+        ),
+      ),
+    );
+  }
+  return contentWidgets;
 }

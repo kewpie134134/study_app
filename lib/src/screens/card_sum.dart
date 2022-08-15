f665cca628ecbc6035df9bb377dac2bf03ca8ea7
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2022-08-15 10:28:14 +0900

    [update] #1 UI を調整

--- lib/src/screens/card_sum.dart
+++ lib/src/screens/card_sum.dart
@@ -6,10 +6,21 @@ class CardSumScreeen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("トランプ足し合わせゲーム")),
-      body: Center(
-        child: Image.asset('images/trumps/card_-.png'),
-      ),
-    );
+        appBar: AppBar(title: const Text("トランプ足し合わせゲーム")),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Image.asset('images/trumps/card_-.png'),
+              ElevatedButton(onPressed: () {}, child: const Text("ゲームスタート")),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(onPressed: () {}, child: const Text("遊び方")),
+                  ElevatedButton(onPressed: () {}, child: const Text("ハイスコア"))
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }

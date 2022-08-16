44d4a1da36fe75c6c8d525111db37c8369eb6aeb
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2022-08-16 13:46:51 +0900

    [add] #1 フローティングボタンを設置

--- lib/src/screens/card_sum.dart
+++ lib/src/screens/card_sum.dart
@@ -12,6 +12,7 @@ class _CardSumScreenState extends State<CardSumScreeen> {
   int counter = 0;
   int sum = 0;
 
+  // ボタンを押したときの挙動
   void _onPressed(String path) {
     setState(() {
       counter += 1;
@@ -22,29 +23,34 @@ class _CardSumScreenState extends State<CardSumScreeen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("トランプ足し合わせゲーム")),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Image.asset(randomAllTrumpMarks[counter]),
-              counter < randomAllTrumpMarks.length - 1
-                  // トランプの配列が 52 枚まで達していないとき
-                  ? ElevatedButton(
-                      onPressed: () {
-                        _onPressed(randomAllTrumpMarks[counter]);
-                      },
-                      child: const Text("ゲームスタート"))
-                  // トランプの枚数が 52 枚に達したとき
-                  : ElevatedButton(onPressed: () {}, child: const Text("終了！！")),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton(onPressed: () {}, child: const Text("遊び方")),
-                  ElevatedButton(onPressed: () {}, child: const Text("ハイスコア"))
-                ],
-              )
-            ],
-          ),
-        ));
+      appBar: AppBar(title: const Text("トランプ足し合わせゲーム")),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Image.asset(randomAllTrumpMarks[counter]),
+            counter < randomAllTrumpMarks.length - 1
+                // トランプの配列が 52 枚まで達していないとき
+                ? ElevatedButton(
+                    onPressed: () {
+                      _onPressed(randomAllTrumpMarks[counter]);
+                    },
+                    child: const Text("ゲームスタート"))
+                // トランプの枚数が 52 枚に達したとき
+                : ElevatedButton(onPressed: () {}, child: const Text("終了！！")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(onPressed: () {}, child: const Text("遊び方")),
+                ElevatedButton(onPressed: () {}, child: const Text("ハイスコア"))
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 }

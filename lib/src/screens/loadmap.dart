aa7feeedb7f7646f55d08a013db4fbf2cdc976e3
Baba, Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2024-02-17 23:30:01 +0900

    [update] #1 ロードマップ画面の処理を関数化

--- lib/src/screens/loadmap.dart
+++ lib/src/screens/loadmap.dart
@@ -15,61 +15,19 @@ List<Widget> _makeWidgets(
     for (int i = 0; i < list.length; i++) {
       if (list[i] == loadmapLength + 1) {
         /// Goal の場合、コンテンツの色を変えて矢印は追加しない
-        tmpWidgets.add(MouseRegion(
-          cursor: SystemMouseCursors.click,
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return LoadmapDetailScreen(
-                    loadmapDetail: list[i].toString()); // ほんとは動的に変更したい
-              }));
-            },
-            child: Container(
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle, color: Colors.red),
-                width: containerSize,
-                height: containerSize),
-          ),
-        ));
+        tmpWidgets.add(_makeLoadmapIcon(
+            context, list[i].toString(), containerSize, Colors.red));
       } else if (list[i] == 0) {
         /// Start の場合、コンテンツの色を変えて矢印は追加しない
-        tmpWidgets.add(MouseRegion(
-            cursor: SystemMouseCursors.click,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                  return LoadmapDetailScreen(
-                      loadmapDetail: list[i].toString()); // ほんとは動的に変更したい
-                }));
-              },
-              child: Container(
-                  decoration: const BoxDecoration(
-                      shape: BoxShape.circle, color: Colors.green),
-                  width: containerSize,
-                  height: containerSize),
-            )));
+        tmpWidgets.add(_makeLoadmapIcon(
+            context, list[i].toString(), containerSize, Colors.green));
         tmpWidgets.add(Icon(
           Icons.arrow_forward,
           size: containerSize,
         ));
       } else {
-        tmpWidgets.add(MouseRegion(
-          cursor: SystemMouseCursors.click,
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return LoadmapDetailScreen(
-                    loadmapDetail: list[i].toString()); // ほんとは動的に変更したい
-              }));
-            },
-            child: Container(
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle, color: Colors.blue),
-                width: containerSize,
-                height: containerSize),
-          ),
-        ));
+        tmpWidgets.add(_makeLoadmapIcon(
+            context, list[i].toString(), containerSize, Colors.blue));
         tmpWidgets.add(Icon(
           Icons.arrow_forward,
           size: containerSize,
@@ -106,6 +64,26 @@ List<dynamic> loadmapList(int loadmapLength) {
     }
   }
   return itemList;
+}
+
+/// コンテナーをクリックした際に loadmapDetailScreen に遷移させる処理を作成
+MouseRegion _makeLoadmapIcon(BuildContext context, String listText,
+    double containerSize, Color containerColor) {
+  return MouseRegion(
+    cursor: SystemMouseCursors.click,
+    child: GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return LoadmapDetailScreen(loadmapDetail: listText); // ほんとは動的に変更したい
+        }));
+      },
+      child: Container(
+          decoration:
+              BoxDecoration(shape: BoxShape.circle, color: containerColor),
+          width: containerSize,
+          height: containerSize),
+    ),
+  );
 }
 
 class LoadmapScreeen extends StatelessWidget {
